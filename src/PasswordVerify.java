3c8497b2e979dc61e231fa25a89273725c5fc84e
saleemsiddique <saleemprogramacion04@gmail.com>
2023-05-08 20:17:21 +0200

    Metodo PasswordVerify COMPLETO

--- src/PasswordVerify.java
+++ src/PasswordVerify.java
@@ -1,3 +1,78 @@
+import java.util.ArrayList;
+
 public class PasswordVerify {
+    public static PasswordResult validaPassword(String password) {
+        String msg = "";
+        boolean isValid = false;
+
+        if (!validaLongitud(password)) {
+            msg += "La contrasenya ha de tenir almenys 8 caràcters";
+        } else {
+            isValid = true;
+        }
+
+        if (!validaNumeros(password)) {
+            msg += "La contrasenya ha de contenir almenys 2 números";
+        } else {
+            isValid = true;
+        }
+
+        if (!validaEspeciales(password)) {
+            msg += "La contrasenya ha de contenir almenys un caràcter especial";
+        } else {
+            isValid = true;
+        }
+
+        if (!validaMayuscula(password)) {
+            msg += "La contrasenya ha de contenir almenys una lletra majuscula";
+        } else {
+            isValid = true;
+        }
+
+        return new PasswordResult(isValid, msg);
+    }
+
+    public static boolean validaLongitud(String contrasenya) {
+        return contrasenya.length() >= 8;
+    }
+
+    public static boolean validaNumeros(String contrasenya) {
+        int contNumeros = 0;
+        boolean esValida = false;
+        for (int i = 0; i < contrasenya.length(); i++) {
+            Character caracter = contrasenya.charAt(i);
+            if (Character.isDigit(caracter)) {
+                contNumeros++;
+            }
+        }
+
+        if (contNumeros >= 2) {
+            esValida = true;
+        }
+        return esValida;
+    }
+
+    public static boolean validaEspeciales(String password) {
+        int contNumeros = 0;
+        int caracteresEspeciales = 0;
+        boolean esValida = false;
+        for (int i = 0; i < password.length(); i++) {
+            Character caracter = password.charAt(i);
+            if (Character.isDigit(caracter)) {
+                contNumeros++;
+            } else if (!Character.isLetterOrDigit(caracter)) {
+                caracteresEspeciales++;
+            }
+        }
+
+        if (caracteresEspeciales >= 1) {
+            esValida = true;
+        }
+        return esValida;
+    }
+
+    public static boolean validaMayuscula(String password){
+        return password.matches(".*[A-Z].*");
+    }
 
 }
--- src/PasswordVerifyTest.java
+++ src/PasswordVerifyTest.java
@@ -5,13 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class PasswordVerifyTest {
 
     @Test
-    public boolean test1(){
+    public void test1(){
         String contraseña = "";
-        PasswordResult resultadoEsperado = new PasswordResult(false, "\"La contraseña tiene que tener al menos 8\n" +
-                "caracteres\\*nLa contraseña tiene que contener al menos 2 números\\*nLa contraseña tiene que\n" +
-                "contener al menos una letra mayúscula\\*nLa contraseña tiene que contener al menos un\n" +
+        PasswordResult resultadoEsperado = new PasswordResult(false, "La contraseña tiene que tener al menos 8" +
+                "caracteresLa contraseña tiene que contener al menos 2 númerosLa contraseña tiene que" +
+                "contener al menos una letra mayúsculaLa contraseña tiene que contener al menos un" +
                 "carácter especial");
-        PasswordResult resultadoReal = validaPassword(contraseña);
-                assertEquals(resultadoEsperado, resultadoReal);
+        PasswordResult resultadoReal = PasswordVerify.validaPassword(contraseña);
+        assertEquals(resultadoEsperado, resultadoReal);
     }
 }
