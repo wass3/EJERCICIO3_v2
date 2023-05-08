93390e7ee762f6c18cafd7931b30e783d6ff07c4
saleemsiddique <saleemprogramacion04@gmail.com>
2023-05-08 20:38:48 +0200

    Metodo PasswordVerify COMPLETO Arreglado

--- src/PasswordVerifyTest.java
+++ src/PasswordVerifyTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PasswordVerifyTest {
+class VerificaContrasenyaTest {
 
     @Test
     public void test1(){
