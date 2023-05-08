242d10846a33256b1e2558f2da50428f79440cad
Wass <unlessbbx03@gmail.com>
2023-05-09 00:05:43 +0200

    Hasta Test 10

--- src/PasswordVerifyTest.java
+++ src/PasswordVerifyTest.java
@@ -96,9 +96,7 @@ class PasswordVerifyTest {
     @Test
     public void test10(){
         String contrasenya = "holacaracola12";
-        PasswordResult resultadoEsperado = new PasswordResult(false, "La contrasenya ha de tenir almenys 8 caracters\n" +
-                "La contrasenya ha de contenir almenys 2 numeros\n" +
-                "La contrasenya ha de contenir almenys una lletra majuscula\n" +
+        PasswordResult resultadoEsperado = new PasswordResult(false, "La contrasenya ha de contenir almenys una lletra majuscula\n" +
                 "La contrasenya ha de contenir almenys un caracter especial\n");
         PasswordResult resultadoReal = PasswordVerify.validaContrasenya(contrasenya);
         assertEquals(resultadoEsperado, resultadoReal);
